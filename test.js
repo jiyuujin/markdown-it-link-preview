b712c5f2541454809190e52ca62450d4d175cfe4
jiyuujin <jiyuujinunite@gmail.com>
2022-09-03 17:02:00 +0900

    chore: esm transpile

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const url = 'https://webneko.dev/'
 
-const linkPreviewPlugin = require('./dist/index.js')
+const linkPreviewPlugin = require('./dist/cjs/index.js')
 
 const md = require('markdown-it')().use(linkPreviewPlugin)
 const result = md.render('[@preview](' + url + ')')
