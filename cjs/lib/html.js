97e6fa76fcf7c60e46b0ee7a60d8ec36335067a9
jiyuujin <jiyuujin@users.noreply.github.com>
2023-10-20 05:50:15 +0000

    deploy: 3aaf774004e6c92e3b9a67a43bc34f65a0ce4541

--- cjs/lib/html.js
+++ cjs/lib/html.js
@@ -2,15 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fetchHtmlSync = void 0;
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-var fetchHtmlSync = function (url) {
+var fetchHtmlSync = function (url, redirectLimit) {
+    if (redirectLimit === void 0) { redirectLimit = 5; }
+    if (redirectLimit <= 0) {
+        throw new Error('Too many redirects');
+    }
     var xhr = new XMLHttpRequest();
     try {
         xhr.open('GET', url, false);
         xhr.withCredentials = true;
+        xhr.followRedirects = true;
         xhr.send();
     }
     catch (err) {
         throw new Error('Failed - ' + err);
+    }
+    if (xhr.readyState === 4 && (xhr.status === 301 || xhr.status === 302)) {
+        var redirectUrl = xhr.getResponseHeader('Location');
+        return (0, exports.fetchHtmlSync)(redirectUrl, redirectLimit - 1);
     }
     if (xhr.status != 200) {
         throw new Error(xhr.status + ' - ' + xhr.statusText ? xhr.statusText : 'Something is wrong.');
--- esm/lib/html.js
+++ esm/lib/html.js
@@ -1,13 +1,21 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-export const fetchHtmlSync = (url) => {
+export const fetchHtmlSync = (url, redirectLimit = 5) => {
+    if (redirectLimit <= 0) {
+        throw new Error('Too many redirects');
+    }
     const xhr = new XMLHttpRequest();
     try {
         xhr.open('GET', url, false);
         xhr.withCredentials = true;
+        xhr.followRedirects = true;
         xhr.send();
     }
     catch (err) {
         throw new Error('Failed - ' + err);
+    }
+    if (xhr.readyState === 4 && (xhr.status === 301 || xhr.status === 302)) {
+        const redirectUrl = xhr.getResponseHeader('Location');
+        return fetchHtmlSync(redirectUrl, redirectLimit - 1);
     }
     if (xhr.status != 200) {
         throw new Error(xhr.status + ' - ' + xhr.statusText ? xhr.statusText : 'Something is wrong.');
